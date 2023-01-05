067b25d2c8c7d8dd0841257b6c2ba51cb7b6b061
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-01-05 14:06:16 +0000

    Update Bhangra.ts

--- assets/tunes/Bhangra.ts
+++ assets/tunes/Bhangra.ts
@@ -62,14 +62,14 @@ export default {
             ot: '                                                                                             A  '
         },
         '7 Break': {
-        ls: "XXXXXXX",
-        ms: "@ls",
-        hs: "@ls",
-        re: "@ls",
-        sn: "@ls",
-        ta: "@ls",
-        ag: "ooooooo",
-        ot: "         A  "
+            ls: "XXXXXXX",
+            ms: "@ls",
+            hs: "@ls",
+            re: "@ls",
+            sn: "@ls",
+            ta: "@ls",
+            ag: "ooooooo",
+            ot: "         A  "
         },
         'Butterfly Break': {
             ls: "XXXXXXX     XXXXXXX     XXXXXXXXXX  XXXXXXX",
