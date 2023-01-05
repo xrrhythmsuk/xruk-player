0f4a2611f323082e6d4566ed17b9471342066257
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-01-05 14:00:23 +0000

    Update Stolen.ts
    Fixed tabs

--- assets/tunes/Stolen.ts
+++ assets/tunes/Stolen.ts
@@ -5,43 +5,43 @@ export default {
     description: require("./stolen.md").default,
     video: undefined,
     patterns: {
-		Tune: {
+        Tune: {
             loop: true,
-			ls: "X  X  X  X  X  X  X  X  X",
-			ms: "      X           X           X  X     X  X",
-			hs: "@ms",
-			re: ".  .  X  .  .  .  X  .  .  .  X  .  .  X  X  .",
-			sn: ".  .  X  .  .  .  X  .  .  .  X  .  .  .  X  .",
-			ta: "X           X           X        X  X     X",
-			ag: "                              o  a     a  o "
-		},
+            ls: "X  X  X  X  X  X  X  X  X",
+            ms: "      X           X           X  X     X  X",
+            hs: "@ms",
+            re: ".  .  X  .  .  .  X  .  .  .  X  .  .  X  X  .",
+            sn: ".  .  X  .  .  .  X  .  .  .  X  .  .  .  X  .",
+            ta: "X           X           X        X  X     X",
+            ag: "                              o  a     a  o "
+        },
         "Break 1": {
-			ls: "                                          X                                               X                                               X                                               X           X                 X                 X ",
-			ms: "@ls",
-			hs: "@ls",
-			re: "X X X X X X X X X X X X X   X     X             X X X X X X X X X X X X X   X     X             X X X X X X X X X X X X X   X     X             X X X X X X X X X X X X X   X     X           XXX               XXX               XXX",
-			sn: "@ls",
-			ta: "@ls",
-			ag: "                                          o                                               o                                               o                                               o           o                 o                 o "
+            ls: "                                          X                                               X                                               X                                               X           X                 X                 X ",
+            ms: "@ls",
+            hs: "@ls",
+            re: "X X X X X X X X X X X X X   X     X             X X X X X X X X X X X X X   X     X             X X X X X X X X X X X X X   X     X             X X X X X X X X X X X X X   X     X           XXX               XXX               XXX",
+            sn: "@ls",
+            ta: "@ls",
+            ag: "                                          o                                               o                                               o                                               o           o                 o                 o "
         },
         "2/4 Break": {
-			ls: "X   X   X X X X ",
-			ms: "@ls",
-			hs: "@ls",
-			re: "@ls",
-			sn: "@ls",
-			ta: "@ls",
-			ag: "o   o   o o o o "
-		},
+            ls: "X   X   X X X X ",
+            ms: "@ls",
+            hs: "@ls",
+            re: "@ls",
+            sn: "@ls",
+            ta: "@ls",
+            ag: "o   o   o o o o "
+        },
         "Progressive Karla (Stolen)": {
-			ls: "X X X X XXXXXXXXXXXXXXXXX       ",
-			ms: "@ls",
-			hs: "@ls",
-			re: "X X X X XXXXXXXXffffffffX",
-			sn: "@re",
-			ta: "@ls",
-			ag: "o o o o ooooooooooooooooo"
-		}
+            ls: "X X X X XXXXXXXXXXXXXXXXX       ",
+            ms: "@ls",
+            hs: "@ls",
+            re: "X X X X XXXXXXXXffffffffX",
+            sn: "@re",
+            ta: "@ls",
+            ag: "o o o o ooooooooooooooooo"
+        }
     },
     exampleSong: ["2/4 Break", "Tune", "Tune", "Break 1", "Tune", "Tune", "Progressive Karla (Stolen)", "Tune", "Tune", "Break 1", "2/4 Break"]
 }
