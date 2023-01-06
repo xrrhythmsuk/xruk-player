89de15f3a2644b89a4ed1f946a4ad07fc2e03344
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-01-06 18:42:17 +0000

    displayName change for 2/4 break
    Seeing if that makes the example song work

--- assets/tunes/Stolen.ts
+++ assets/tunes/Stolen.ts
@@ -24,7 +24,8 @@ export default {
             ta: "@ls",
             ag: "                                          o                                               o                                               o                                               o           o                 o                 o "
         },
-        "2/4 Break": {
+        "24 Break": {
+            displayName: '2/4 Break',
             ls: "X   X   X X X X ",
             ms: "@ls",
             hs: "@ls",
@@ -52,5 +53,5 @@ export default {
             ag: "o o o o ooooooooooooooooo"
         }
     },
-    exampleSong: ["2/4 Break", "Tune", "Tune", "4 Hits (Stolen)", "Tune", "Tune", "Break 1", "Tune", "Tune", "Progressive Karla (Stolen)", "Tune", "Tune", "2/4 Break"]
+    exampleSong: ["24 Break", "Tune", "Tune", "4 Hits (Stolen)", "Tune", "Tune", "Break 1", "Tune", "Tune", "Progressive Karla (Stolen)", "Tune", "Tune", "24 Break"]
 }
