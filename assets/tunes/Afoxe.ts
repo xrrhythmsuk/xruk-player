86568571b067061672facb2e74e10c4289e5d43d
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-03-22 21:37:14 +0000

    Individual parts
    Moved before breaks instead of after them

--- assets/tunes/Afoxe.ts
+++ assets/tunes/Afoxe.ts
@@ -5,7 +5,7 @@ export default {
     sheet: require("./afoxe.pdf"),
     description: require("./afoxe.md").default,
     patterns: {
-        "Tune": {
+        Tune: {
             loop: true,
             ls: 's   s   s   s   s   s   X   X   ',
             ms: '0     X 0     X 0     X X X X X ',
@@ -16,6 +16,38 @@ export default {
             ag: 'a a o o aa o oo a a o o aa o oo ',
             sh: '................................'
         },
+        "Low Surdo": {
+            loop: true,
+            ls: 's   s   s   s   s   s   X   X   ',
+            sh: 'X   .   .   .   X   .   .   .'
+        },
+        "Mid/High Surdo": {
+            loop: true,
+            ms: '0     X 0     X 0     X X X X X ',
+            hs: '@ms',
+            sh: 'X   .   .   .   X   .   .   .'
+        },
+        "Repi": {
+            loop: true,
+            re: 'f  hs r f  hs r f  hs r s r s r ',
+            sh: 'X   .   .   .   X   .   .   .'
+        },
+        "Snare": {
+            loop: true,
+            sn: 'X...X..XX..X....',
+            sh: 'X   .   .   .   '
+        },
+        "Tam": {
+            loop: true,
+            ta: 'X X X X XX XX X ',
+            sh: 'X   .   .   .   '
+        },
+        "Agogo": {
+            displayName: 'Agogô',
+            loop: true,
+            ag: 'a a o o aa o oo ',
+            sh: 'X   .   .   .   '
+        }
         "Break 1": {
             ls: 'X       X       X       X XXXXX ',
             ms: '@ls',
@@ -77,38 +109,6 @@ export default {
             ta: '@ls',
             ag: '@ls',
             sh: '@ls'
-        },
-        "Low Surdo": {
-            loop: true,
-            ls: 's   s   s   s   s   s   X   X   ',
-            sh: 'X   .   .   .   X   .   .   .'
-        },
-        "Mid/High Surdo": {
-            loop: true,
-            ms: '0     X 0     X 0     X X X X X ',
-            hs: '@ms',
-            sh: 'X   .   .   .   X   .   .   .'
-        },
-        "Repi": {
-            loop: true,
-            re: 'f  hs r f  hs r f  hs r s r s r ',
-            sh: 'X   .   .   .   X   .   .   .'
-        },
-        "Snare": {
-            loop: true,
-            sn: 'X...X..XX..X....',
-            sh: 'X   .   .   .   '
-        },
-        "Tam": {
-            loop: true,
-            ta: 'X X X X XX XX X ',
-            sh: 'X   .   .   .   '
-        },
-        "Agogo": {
-            displayName: 'Agogô',
-            loop: true,
-            ag: 'a a o o aa o oo ',
-            sh: 'X   .   .   .   '
         }
     },
     exampleSong: ["Tune", "Tune", "Break 1", "Tune", "Tune", "Long call", "Tune", "Tune", "Tam Stroke", "Tune", "Tune", "Break 2", "Tune", "Tune", "Break 3"]
