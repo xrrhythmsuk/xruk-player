d14b1a21b1f62b3c16745ad6cf4ffaf6686b1a3b
Tim <user@server.fake>
2023-04-10 16:51:32 +0100

    Instrument buttons

---
+++ src/ui/instrument/instrument-buttons.vue
@@ -0,0 +1,12 @@
+<div class="bb-instr-buttons">
+    <fa icon="headphones"/>&nbsp;
+    <div class="btn-group">
+        <button data-toggle="button" class="btn btn-outline-secondary" :class="{active: active('ls')}" @click="onClick('ls')">Low</button>
+        <button data-toggle="button" class="btn btn-outline-secondary" :class="{active: active('ms')}" @click="onClick('ms')">Mid</button>
+        <button data-toggle="button" class="btn btn-outline-secondary" :class="{active: active('hs')}" @click="onClick('hs')">High</button>
+        <button data-toggle="button" class="btn btn-outline-secondary" :class="{active: active('sn')}" @click="onClick('sn')">Snare</button>
+        <button data-toggle="button" class="btn btn-outline-secondary" :class="{active: active('re')}" @click="onClick('re')">Repi</button>
+        <button data-toggle="button" class="btn btn-outline-secondary" :class="{active: active('ta')}" @click="onClick('ta')">Tam</button>
+        <button data-toggle="button" class="btn btn-outline-secondary" :class="{active: active('ag')}" @click="onClick('ag')">Agogô</button>
+    </div>
+</div>
--- src/ui/tune-info/tune-info.vue
+++ src/ui/tune-info/tune-info.vue
@@ -1,8 +1,11 @@
 <div class="bb-tune-info" v-if="tune">
-	<h1 v-if="tune.description || tune.sheet" class="d-flex align-items-center">
-		<span class="flex-grow-1">{{tune.displayName || tuneName}}</span>
-		<PlaybackSettings :playback-settings="playbackSettings" :default-speed="tune.speed" v-if="tune.exampleSong" />
-	</h1>
+	<div class="d-flex flex-wrap">
+		<h1 class="flex-grow-1">{{tune.displayName || tuneName}}</h1>
+		<div class="d-flex mb-4">
+			<InstrumentButtons :playback-settings="playbackSettings"/>
+			<PlaybackSettings :playback-settings="playbackSettings" :default-speed="tune.speed" v-if="tune.exampleSong" />
+		</div>
+	</div>
 
 <ExampleSongPlayer
 	v-if="tune.exampleSong"
