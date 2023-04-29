bcdc95cab32eab461913ff5c46bee033407ba3f2
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-04-29 16:45:50 +0100

    Create Jericho.ts

---
+++ assets/tunes/Jericho.ts
@@ -0,0 +1,62 @@
+import { repeat } from "../../src/tuneHelper";
+export default {
+    categories: ["new", "tricky"],
+    speed: 140,
+    time: 3,
+    sheet: require("./jericho.pdf"),
+    description: require("./jericho.md").default,
+    video: undefined,
+	
+    patterns: {
+        Tune: {
+            loop: true,
+            ls: "X X0  X X0  X X0 X   0  X X0 X   0  X X0 X   0",
+            ms: "   X X   X X   0     X X   0     X X   0     X X",
+            hs: "@ms",
+            re: "f  h sf  h sf  h sf  h sf  h sf  h sf  h sX r",
+            sn: "...X.X...X.....X.X...X.....X.X...X.....X.X..XX.X",
+            ta: "   X     X     X X   X     X X   X     X X   X",
+            ag: "o  a ao  a ao a  o      o a  o      o a  o",
+            sh: "X  X .X  X .X  X .X  X .X  X .X  X .X  X .X  X ."
+        },
+        'Break 1': {
+            upbeat: 3,
+            ls: "X XX  X  X XX X",
+            ms: "@ls",
+            hs: "@ls",
+            re: "X XX  X  X XX X     X  X  X",
+            sn: "@ls",
+            ta: "@ls",
+            ag: "@ls"
+        },
+        'Break 2': {
+            upbeat: 1,
+            ls: " " + repeat(4, "                  X     "),
+            ms: "@ls",
+            hs: "@ls",
+            re: " " + repeat(4, "f  h sf  h sf  h sX r   "),
+            sn: "@ls",
+            ta: "X" + repeat(2, "X XX  X X  X            X    XX XX X           X"),
+            ag: "a" + repeat(2, "a aa  a o  a            a    aa ao a           a")
+        },
+        'Break 3': {
+            ls: "s  s  s  s  s  s  s  s  s  s  s  s  s  s  s  s",
+            ms: "@ls",
+            hs: "@ls",
+            re: "        X        X  X           X        X  X  X",
+            sn: "@re",
+            ta: "@re",
+            ag: "@re"
+        },
+        'Call Break': {
+            ls: "                     X           X           X                       X XX  X  X XX X",
+            ms: "@ls",
+            hs: "@ls",
+            re: "X XX XX XX  X X  X      X X  X      X X  X      X XX XX XX  X X  X   X XX  X  X XX X     X  X  X",
+            sn: "@ls",
+            ta: "@ls",
+            ag: "@ls"
+        }
+    },
+    exampleSong: [ { tuneName: "General Breaks", patternName: "Whistle in" }, "Call Break", "Tune", "Tune", "Break 1", "Tune", "Break 2", "Tune", "Break 3"]
+}
