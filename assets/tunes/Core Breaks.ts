ca9f21ec000b8c0a92ef57b03984b1ad4b9284fd
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-07-25 18:52:17 +0100

    Core Breaks as "breaks"

--- assets/tunes/Core Breaks.ts
+++ assets/tunes/Core Breaks.ts
@@ -1,7 +1,7 @@
 import { crescendo, decrescendo, repeat, stretch } from "../../src/tuneHelper";
 
 export default {
-    categories: ["common", "onesurdo", "easy", "medium", "tricky"],
+    categories: ["breaks", "onesurdo", "easy", "medium", "tricky"],
     sheet: require("./general-breaks.pdf"),
     description: require("./core-breaks.md").default,
     video: undefined,
