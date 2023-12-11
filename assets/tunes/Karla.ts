4e6ee6e4da3f111abd689022a0611a83cd6315a6
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-12-11 22:10:38 +0000

    Update Karla.ts

--- assets/tunes/Karla.ts
+++ assets/tunes/Karla.ts
@@ -13,7 +13,16 @@ export default {
             sn: '....X.......X.......X.......X.......X.......X.......X.......X...',
             ta: '    X       X       X  X X XX       X       X       X  X X XX   ',
             ag: 'o  oa o o  oa o o  oa o o  oa o o  oa o o  oa o o  oa o o  oa o ',
-            sh: '................................................................'
+            sh: '................................................................',
+            mnemonics: {
+                ls: "Ra-bbit run Ra-bbit run Ra-bbit run Ra-bbit run from the ca-ges",
+                ms: "Run fast Run fast Run fast Run fast from the ca-ges",
+                re: "Ka-rla said you won't be fur coats Ka-rla said you won't be fur coats"
+                    + "Ka-rla said you won't be fur coats Ka-rla said you won't be fur coats",
+                ta: "Ka-rla sa-ved us from the farm Ka-rla sa-ved us from the farm",
+                ag: "Ha-ppy bu-nnies ha-ppy bu-nnies Ha-ppy bu-nnies ha-ppy bu-nnies"
+                    + "Ha-ppy bu-nnies ha-ppy bu-nnies Ha-ppy bu-nnies ha-ppy bu-nnies"
+            }
         },
         'Karla Break (Karla)': {
             ls: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX               ',
@@ -33,7 +42,11 @@ export default {
             sn: '@re',
             ta: '@re',
             ag: '@re',
-            sh: '@re'
+            sh: '@re',
+            mnemonics: {
+                ls: "A-ny-bo-dy a-ny-bo-dy a-ny-bo-dy a-ny-bo-dy Got some le-ttuce? I have I have I have I have",
+                re: "A-ny-bo-dy a-ny-bo-dy a-ny-bo-dy a-ny-bo-dy Got some le-ttuce? Great We're star-ving here Great We're star-ving here"
+            }
         },
         'Break 2 Inverted': {
             ls: 'XXXXXXXXXXXXXXXXX   X   X   X   X X    X X      X X    X X      X X    X X      X X    X X      X   X   X   X   XXXXXXXXXXXXXXXX',
@@ -43,7 +56,13 @@ export default {
             sn: '@re',
             ta: '@re',
             ag: '@re',
-            sh: '@re'
+            sh: '@re',
+            mnemonics: {
+                ls: "A-ny-bo-dy a-ny-bo-dy a-ny-bo-dy a-ny-bo-dy Got some le-ttuce? I have I have I have I have"
+                    + "I have I have I have I have Got some le-ttuce A-ny-bo-dy a-ny-bo-dy a-ny-bo-dy a-ny-bo-dy?",
+                re: "A-ny-bo-dy a-ny-bo-dy a-ny-bo-dy a-ny-bo-dy Got some le-ttuce? Great We're star-ving here Great We're star-ving here"
+                    + "Great We're star-ving here Great We're star-ving here Got some le-ttuce A-ny-bo-dy a-ny-bo-dy a-ny-bo-dy a-ny-bo-dy?"
+            }
         }
     },
     exampleSong: [ { tuneName: "Core Breaks", patternName: "Whistle in" }, "Tune", "Karla Break (Karla)", "Tune", "Break 2", "Tune", "Break 2 Inverted", "Tune"]
