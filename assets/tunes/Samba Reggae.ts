864fbcfa8308554359802ce62afb2a5e88827ecd
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-12-11 22:36:26 +0000

    Update Samba Reggae.ts

--- assets/tunes/Samba Reggae.ts
+++ assets/tunes/Samba Reggae.ts
@@ -14,7 +14,16 @@ export default {
             sn: 'X..X..X...X..X..',
             ta: 'X  X  X   X X   ',
             ag: 'o a a oo a aa o ',
-            sh: '................'
+            sh: '................',
+            mnemonics: {
+                ls: "I quite like re-ggae",
+                ms: "Sam-ba re-ggae",
+                hs: "Time to play sam-ba re-ggae",
+                re: "Can I Get a Bit of Re-ggae",
+                sn: "Bo . . ssa . . for . . . re- . . ggae . .",
+                ta: "Cla-ve for re-ggae",
+                ag: "Sam-ba re-ggae on the a-go-gô"
+            }
         },
         'Break 1': {
             ls: '                X X XX XX                       X  X  X X                                  XX                              XX                              XX               XXXX',
@@ -24,7 +33,13 @@ export default {
             sn: '                X X XX XX                       X  X  X X       X..X..X.X..X..X.X..X..X.X       X..X..X.X..X..X.X..X..X.X       X..X..X.X..X..X.X..X..X.X       X  X  X   X     ',
             ta: '@ls',
             ag: '@ls',
-            sh: '@ls'
+            sh: '@ls',
+            mnemonics: {
+                ls: "We want some-thing to eat We want some grub Yum yum Yum yum Yum yum Sam-ba re-ggae",
+                re: repeat(2, "What do you want, tell me what do you want? ") + "Yum yum Yum yum Yum yum Sam-ba re-ggae",
+                sn: "We want some-thing to eat We want some grub"
+                    + repeat(3, repeat(3, "Sau-sa-ges sau-sa-ges I've got ") + "in the pan ") + "Sam-ba re-ggae"
+            }
         },
         'Break 2': {
             ls: '            XXXX            XXXX            XXXX            XXXX',
@@ -34,7 +49,11 @@ export default {
             sn: '@ls',
             ta: '@ls',
             ag: '@ls',
-            sh: '@ls'
+            sh: '@ls',
+            mnemonics: {
+                ls: repeat(4, "Sam-ba re-ggae "),
+                re: repeat(4, "Sam-ba re-ggae Sa")
+            }
         },
         'Whistle Break': {
             loop: true,
@@ -46,6 +65,10 @@ export default {
             sn: "@re",
             ta: "@re",
             ag: "@re",
+            mnemonics: {
+                ls: "Time to do the Sheff whi-stle break",
+                re: "Yep Yep Yep Yep"
+            }
         },
         'Break 3': {
             ls: '                X  XX X XX XX X                 X  XX X XX XX X                 X  XX X X       X  XX X X       X  X  X         ',
@@ -55,7 +78,13 @@ export default {
             sn: 'X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...X...',
             ta: '                                X  X  X   X                     X  X  X   X                 X X             X X             XXXX',
             ag: '                                X  X  X   X                     X  X  X   X                 a a             a a             aaaa',
-            sh: '                                X  X  X   X                     X  X  X   X                                                 XXXX'
+            sh: '                                X  X  X   X                     X  X  X   X                                                 XXXX',
+            mnemonics: {
+                ls: repeat(2, "Do you want to play sam-ba reggae? ") + repeat(2, "Do you want to play? ") + "Right let's play",
+                re: repeat(2, "Sam-ba re-ggae ") + repeat(2, "z We want to ") + "Sam-ba re-ggae",
+                ta: repeat(2, "Sam-ba re-ggae ") + repeat(2, "want to ") + "Sam-ba re-ggae",
+                ag: repeat(2, "Sam-ba re-ggae ") + repeat(2, "want to ") + "Sam-ba re-ggae"
+            }
         },
         'Call Break': {
             ls: '          X X             X X             X X                                                                 X ',
@@ -65,7 +94,14 @@ export default {
             sn: '          X X             X X             X X                   X..X..X...X.X...X..X..X...X.X...X..X..X...X.X...',
             ta: '          X X             X X             X X   X  X  X   X X   X  X  X   X X   X  X  X   X X   X  X  X   X X   ',
             ag: '@ms',
-            sh: '@ms'
+            sh: '@ms',
+            mnemonics: {
+                ls: repeat(3, "Act now ") + ".",
+                ms: repeat(3, "Act now "),
+                re: repeat(3, "There's no time to waste so "),
+                sn: repeat(3, "Act now ") + repeat(3, "Shut . . the . . sy- . . . stem . down . . . ")
+                ta: repeat(3, "Act now ") + repeat(4, "Shut the sy- stem down ")
+            }
         },
         'SOS Break': {
             ls: 'X       X       X       X       X       X       X       X     X ',
@@ -75,7 +111,12 @@ export default {
             sn: '@re',
             ta: '@re',
             ag: '@re',
-            sh: '@re'
+            sh: '@re',
+            mnemonics: {
+                ls: repeat(4, "Act Now ") + ".",
+                ms: repeat(4, "Act Now "),
+                re: repeat(4, "No time to waste Act now ")
+            }
         },
         'Tune (SOS Repi)': {
             loop: true,
