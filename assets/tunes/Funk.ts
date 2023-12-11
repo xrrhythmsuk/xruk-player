4c96bc3228ab1dc260a5edee921e0028799507e3
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-12-11 21:46:41 +0000

    Update Funk.ts

--- assets/tunes/Funk.ts
+++ assets/tunes/Funk.ts
@@ -14,18 +14,17 @@ export default {
             ag: 'o  a  o   a a a o  a  o   a a a ',
             sh: 'X.X.X.X.X.X.X.X.X.X.X.X.X.X.X.X.',
             mnemonics: {
-                ls: 'I like po-ta-toes I like them mashed',
-                ms: "Oh. Do you? Yes. I do",
-                hs: "Oh. Do you? Yes. I do",
-                re: "Lots and lots and lots and lots and lots and lots and lots, e-nough for din-ner",
-               ag: 'I like hot po-ta-to I like hot po-ta-to',
-               ta: "Oh. Do you? Yes. I do",
+                ls: "I like po-ta-toes, I like them mashed",
+                ms: "Oh, do you? 'Cos I don't",
+                re: "Mash is fine, but Mash is fine but Mash is fine but Chips are clea-rly be-tter",
+                ag: "I like hash browns be-tter I like hash browns be-tter",
             }
         },
        "Alt Repi": {
-            loop: true,
-            re: '  X   X XXXXX X   X   X X XX XX ',
-            sh: 'X   .   .   .   X   .   .   .   '
+           loop: true,
+           re: '  X   X XXXXX X   X   X X XX XX ',
+           sh: 'X   .   .   .   X   .   .   .   ',
+           mnemonics: {  re: "I don't want to come off pi-cky, But you can't beat sa-lty chips" }
         },
         "Break 1": {
             ls: 'X X     X X   X X X     X       X X     X X   X X X     X       ',
@@ -35,7 +34,11 @@ export default {
             sn: '@re',
             ta: '@re',
             ag: '@re',
-            sh: '@re'
+            sh: '@re',
+            mnemonics: {
+                ls: "Boil them Fry them Just give us Spuds Boil them Fry them Just give us Spuds",
+                re: "Mash them Up Give us With salt Mash them Up Give us On a plate"
+            }
         },
         "Break 2": {
             ls: 'X X X X X X X X ',
@@ -45,7 +48,8 @@ export default {
             sn: '@ls',
             ta: '@ls',
             ag: '@ls',
-            sh: '@ls'
+            sh: '@ls',
+            mnemonics: {  ls: "Hu-rry up, we're blee-ding star-ving" }
         }
     },
     exampleSong: [ { tuneName: "Core Breaks", patternName: "Whistle in" }, "Tune", "Tune", "Break 1", "Tune", "Tune", "Break 2", "Tune", "Tune"]
