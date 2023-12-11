162526143863a21c9e1a830abd2b6202220d98cc
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-12-11 22:47:03 +0000

    Update Sambasso.ts

--- assets/tunes/Sambasso.ts
+++ assets/tunes/Sambasso.ts
@@ -15,12 +15,20 @@ export default {
             sn: 'X..X..X...X..X..',
             ta: 'X X X   XX XX   ',
             ag: 'o  aa oo a oo a ',
-            sh: 'X.X.X.X.X.X.X.X.'
+            sh: 'X.X.X.X.X.X.X.X.',
+            mnemonics: {
+                ls: repeat(2, "Hump-ty Dump-ty "),
+                re: "Hump . . ty's . . on . . the wall . . a-gain .",
+                sn: "Bo . . ssa . . for . . . Hump . . ty . .",
+                ta: "Hump-ty mate, get off the wall",
+                ag: "Hump-ty Dump-ty fell off a brick wall"
+            }
         },
         'Alt Tam': {
             loop: true,
             ta: ' X XX X XX XX  X X XXXX X  XX   ',
-            sh: 'X   .   .   .   X   .   .   .   '
+            sh: 'X   .   .   .   X   .   .   .   ',
+            mnemonics: {  ta: "Hump-ty Dump-ty sat on the wall, which made his hae-mo-rrhoids worse and all" }
         },
         'Break 1': {
             ls: 'X  rX r XX XX   ',
@@ -31,7 +39,15 @@ export default {
             ta: 'X X X   XX XX   ',
             ag: 'o  aa ooXX XX   ',
             sh: 'X X X X XX XX   ',
-            ot: 'y w w           '
+            ot: 'y w w           ',
+            mnemonics: {
+                ls: "Hump-ty Dump-ty Get off the wall",
+                re: "Hump . . ty's . . on . Get off the wall",
+                sn: "Bo . . ssa . . for . Get off the wall",
+                ta: "Hump-ty mate, Get off the wall",
+                ag: "Hump-ty Dump-ty fell Get off the wall",
+                ot: "Hump-ty mate"
+            }
         },
         'Break 2': {
             ls: repeat(4, 'X X X X X       '),
@@ -41,7 +57,11 @@ export default {
             sn: '@re',
             ta: '@re',
             ag: '@re',
-            sh: '@re'
+            sh: '@re',
+            mnemonics: {
+                ls: repeat(4, "Put that ladder down "),
+                re: repeat(4, "Get off the wall ")
+            }
         },
         'Call Break (Intro)': {
             upbeat: 1,
@@ -52,7 +72,11 @@ export default {
             sn: '@ls',
             ta: '@ls',
             ag: '@ls',
-            sh: '@ls'
+            sh: '@ls',
+            mnemonics: {
+                ls: repeat(4, "Get off the wall ") + repeat(3, "We're so sick of glu-ing his stu-pid shell back on ") + "We're so sick of glu-ing his stu-pid shell back",
+                re: repeat(4, "z Hump-ty mate " + repeat(4, "I . . tried . . but . . he . . said it was made up")
+            }
         }
     },
     exampleSong: [ { tuneName: "Core Breaks", patternName: "Whistle in" }, "Tune", "Tune", "Tune", "Tune", "Break 1", "Tune", "Tune", "Tune", "Tune", "Break 2", "Tune", "Tune", "Tune", "Tune", "Call Break (Intro)"]
