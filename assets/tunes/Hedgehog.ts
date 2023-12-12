caca1db27021559ac20a80b18282e0657a12c027
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-12-12 15:08:29 +0000

    Update Hedgehog.ts

--- assets/tunes/Hedgehog.ts
+++ assets/tunes/Hedgehog.ts
@@ -16,7 +16,7 @@ export default {
             mnemonics: {
                 ls: "Hedge-hog Hedge-hog Hedge-hog I'm a hedge-hog",
                 ms: "I'm a hedge-hog I'm a hedge-hog",
-                hs: "Hege-hog Hedge-hog Hedge-hog a hog",
+                hs: "Hedge-hog Hedge-hog Hedge-hog A hog",
                 re: "1 hedge-hog, 2 hedge-hog, 3 hedge-hog I'm a hedge-hog",
                 sn: "Hedge . . hog . . a Hedge . . hog . . a . Hedge . . hog . a . Small . . . guy . . .",
                 ta: "Hedge-hog Hedge-hog Hedge-hog I'm a hog",
