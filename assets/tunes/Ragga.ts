f351f9e763a7c396afd499eee1815aa6478808de
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2024-09-07 17:05:32 +0100

    Update Ragga.ts
    Replaced tabs with 4 spaces

--- assets/tunes/Ragga.ts
+++ assets/tunes/Ragga.ts
@@ -5,32 +5,32 @@ export default {
     sheet: require("./ragga.pdf"),
     description: require("./ragga.md").default,
     patterns: {
-		Tune: {
-			loop: true,
-			ls: repeat(4, 'X  X  0 '),
-			ms: repeat(4, '0  X  X '),
-			hs: '@ms',
-			re: repeat(3, '  X   X ') + ' XXX  X ',
-			sn: repeat(4, '..XX..X.'),
-			ta: repeat(3, '  X   X ') + '  XX  X ',
-			ag: 'o a o a oa ao a o a  o oo a o   ',
-			sh: repeat(4, 'X.X.X.X.'),
-			mnemonics: {
-				ls: repeat(4, "Ra-gga yeah "),
-				ms: repeat(4, "It's ra-gga "),
-				re: repeat(3, "Ra-gga ") + "So turn it up",
-				sn: repeat(4, ". . Turn it . . up . "),
-				ta: repeat(3, "Ra-gga ") + "Turn it up",
-				ag: "What's that pla-ying down at the dance-hall? Ra-gga? Be-tter play it loud"
-			}
-		},
-		'Starter Agogo': {
-			displayName: 'Starter Agogô',
-			loop: true,
-			ag: 'o a o a o ao ao ',
-			sh: 'X   .   .   .   ',
-			mnemonics: {  ag: '1 2 3 4 Put some ra-gga on'}
-		},
+        Tune: {
+            loop: true,
+            ls: repeat(4, 'X  X  0 '),
+            ms: repeat(4, '0  X  X '),
+            hs: '@ms',
+            re: repeat(3, '  X   X ') + ' XXX  X ',
+            sn: repeat(4, '..XX..X.'),
+            ta: repeat(3, '  X   X ') + '  XX  X ',
+            ag: 'o a o a oa ao a o a  o oo a o   ',
+            sh: repeat(4, 'X.X.X.X.'),
+            mnemonics: {
+                ls: repeat(4, "Ra-gga yeah "),
+                ms: repeat(4, "It's ra-gga "),
+                re: repeat(3, "Ra-gga ") + "So turn it up",
+                sn: repeat(4, ". . Turn it . . up . "),
+                ta: repeat(3, "Ra-gga ") + "Turn it up",
+                ag: "What's that pla-ying down at the dance-hall? Ra-gga? Be-tter play it loud"
+            }
+        },
+        'Starter Agogo': {
+            displayName: 'Starter Agogô',
+            loop: true,
+            ag: 'o a o a o ao ao ',
+            sh: 'X   .   .   .   ',
+            mnemonics: {  ag: '1 2 3 4 Put some ra-gga on'}
+        },
         'Kickback 1': {
             loop: true,
             ls: 'X  X    X  X    ',
@@ -62,50 +62,50 @@ export default {
             }
         },
         'Break 1': {
-        	ls: '        X  X  X',
-        	ms: '@ls',
-        	hs: '@ls',
-        	re: '          X  X ',
+            ls: '        X  X  X',
+            ms: '@ls',
+            hs: '@ls',
+            re: '          X  X ',
             sn: '@re',
-        	ta: '@re',
-        	ag: '@re',
-        	sh: '@re',
-        	ot: 'w w w w        ',
-        	mnemonics: {
-        		ls: 'Put ra-on',
-        		re: 'some gga',
-        		ot: '1 2 3 4'
-        	}
+            ta: '@re',
+            ag: '@re',
+            sh: '@re',
+            ot: 'w w w w        ',
+            mnemonics: {
+                ls: 'Put ra-on',
+                re: 'some gga',
+                ot: '1 2 3 4'
+            }
         },
-		'Break 2': {
-			ls: 'X           XXX ',
-			ms: '@ls',
-			hs: '@ls',
-			re: '@ls',
-			sn: '@ls',
-			ta: '@ls',
-			ag: '@ls',
-			sh: '@ls',
-			ot: '  A A A A A     ',
-			mnemonics: {
-				ls: 'Hey se-ven eight',
-				ot: '2 3 4 5 6'
-			}
-		},
-		'Break 3': {
-			ls: 'X  X  X         ',
-			ms: '@ls',
-			hs: '@ls',
-			re: '        X  X  X ',
-			sn: '@re',
-			ta: '@re',
-			ag: '@re',
-			sh: '@re',
-			mnemonics: {
-				ls: '3 2 1',
-				re: '3 2 1'
-			}
-		},
+        'Break 2': {
+            ls: 'X           XXX ',
+            ms: '@ls',
+            hs: '@ls',
+            re: '@ls',
+            sn: '@ls',
+            ta: '@ls',
+            ag: '@ls',
+            sh: '@ls',
+            ot: '  A A A A A     ',
+            mnemonics: {
+                ls: 'Hey se-ven eight',
+                ot: '2 3 4 5 6'
+            }
+        },
+        'Break 3': {
+            ls: 'X  X  X         ',
+            ms: '@ls',
+            hs: '@ls',
+            re: '        X  X  X ',
+            sn: '@re',
+            ta: '@re',
+            ag: '@re',
+            sh: '@re',
+            mnemonics: {
+                ls: '3 2 1',
+                re: '3 2 1'
+            }
+        },
         'Zorro Break': {
             loop: true,
             ls: 'X       X       X       X  X  X ',
@@ -119,17 +119,17 @@ export default {
             mnemonics: {  ls: "Ha, take that Zo-rro break" }
         },
         'Tune Surdos Zorro': {
-        	displayName: 'Tune (Surdos Zorro Break)',
-        	loop: true,
+            displayName: 'Tune (Surdos Zorro Break)',
+            loop: true,
             ls: 'X       X       X       X  X  X ',
             ms: '@ls',
             hs: '@ls',
-			re: repeat(3, '  X   X ') + ' XXX  X ',
-			sn: repeat(4, '..XX..X.'),
-			ta: repeat(3, '  X   X ') + '  XX  X ',
-			ag: 'o a o a oa ao a o a  o oo a o   ',
-			sh: repeat(4, 'X.X.X.X.'),
+            re: repeat(3, '  X   X ') + ' XXX  X ',
+            sn: repeat(4, '..XX..X.'),
+            ta: repeat(3, '  X   X ') + '  XX  X ',
+            ag: 'o a o a oa ao a o a  o oo a o   ',
+            sh: repeat(4, 'X.X.X.X.'),
         }
-	},
-	exampleSong: [ { tuneName: "Core Breaks", patternName: "Whistle in" }, "Tune", "Tune", "Kickback 1", "Kickback 1", "Kickback 1", "Kickback 1", "Kickback 2", "Kickback 2", "Kickback 2", "Kickback 2", "Break 2", "Tune", "Tune", "Break 1", "Tune", "Tune", "Break 2", "Tune", "Tune", "Break 3", "Tune", "Tune", "Tune Surdos Zorro", "Tune Surdos Zorro", "Zorro Break", "Zorro Break", "Break 1" ]
+    },
+    exampleSong: [ { tuneName: "Core Breaks", patternName: "Whistle in" }, "Tune", "Tune", "Kickback 1", "Kickback 1", "Kickback 1", "Kickback 1", "Kickback 2", "Kickback 2", "Kickback 2", "Kickback 2", "Break 2", "Tune", "Tune", "Break 1", "Tune", "Tune", "Break 2", "Tune", "Tune", "Break 3", "Tune", "Tune", "Tune Surdos Zorro", "Tune Surdos Zorro", "Zorro Break", "Zorro Break", "Break 1" ]
 }
