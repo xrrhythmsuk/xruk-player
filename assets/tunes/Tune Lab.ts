23c203a5710e9525f8d676aae028c44b98ef6bef
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2026-03-02 17:06:05 +0000

    Typos in Tune Lab.ts

--- assets/tunes/Tune Lab.ts
+++ assets/tunes/Tune Lab.ts
@@ -1,6 +1,7 @@
 import {  repeat } from "../../src/tuneHelper";
 
 export default {
+	displayName: "... from the Tune Lab",
     categories: ["new", "tricky", "proposed"],
     sheet: undefined,
     description: require("./tune-lab.md").default,
@@ -25,36 +26,40 @@ export default {
                 ag: repeat(2, "Don't wait for an an-gel to app-ear ")
             }
         },
-        'Angel Break 1': {
+        'Angel, Break 1': {
             ls: 'X X X X XX X XX',
             ms: '@ls',
             hs: '@ls',
             re: '@ls',
+			sn: '@ls',
             ta: '@ls',
             ag: '@ls',
             sh: '@ls',
             mnemonics: {  ls: "Don't wait for an an-gel to app-ear" }
         },
-        'Angel Break 2': {
+        'Angel, Break 2': {
             ls: 'X   X     XX    ',
             ms: '@ls',
             hs: '@ls',
             sh: 'X   .   .   .   ',
-            mnemonics: {  ls: 'Ha-loes are go' }
+            mnemonics: {  ls: 'Ha-loes Let's go' }
         },
         'Pancake Break': {
             loop: true,
-            re: 'xxx xxx xxx xxx   xxx xxx xxx xx',
+            re: 'XXX XXX XXX XXX   XXX XXX XXX  ',
             sh: 'X   .   .   .',
-            mnemonics: {  re: repeat(4, "Going to flip ") + repeat(4, "Going to flop ") }
+            mnemonics: {  re: repeat(4, "Going to flip ") + repeat(3, "Going to flop ") }
         },
         'Whoopi Break': {
             loop: true,
-            ta: '    X X     X   ',
+            re: '    X X     X   ',
+			sn: '@re',
+			ta: '@re',
+			ag: '@re',
             sh: 'X   .   .   .   ',
-            mnemonics: {  ta: 'Clapp-ing hands' }
+            mnemonics: {  re: 'Clapp-ing hands' }
         },
-        'Funk Break 1 (Conga)': {
+        'Funk, Break 1 (Conga)': {
             loop: true,
             ls: 'X X     X X   X X X     X       X   X   X   X   X   X   X   X   ',
             ms: '@ls',
@@ -62,7 +67,7 @@ export default {
             sh: 'X   .   .   .   X   .   .   .   X   .   .   .   X   .   .   .   ',
             mnemonics: {  ls: "Boil them Mash them Just give us spuds Ve-ry ta-sty ve-ry ta-sty" }
         },
-        'Forest Thump': {
+        'Forest Thump (Draft)': {
             loop: true,
             ls: 'X   r   XX XX X X   r   XX XX X X   r   XX XX X X   r   X  X  X',
             ms: 'X XX XX X   r X X XX XX X   r X X XX XX X   r X X   r   X  X  X',
@@ -77,7 +82,7 @@ export default {
                 ms: repeat(3, "We can play all day long be-cause ") + "We are For-est Thump"
             }
         },
-		'Forest Thump Break 1': {
+		'Forest Thump (Draft), Break 1': {
             ls: 'X XX XX X  X  X',
             ms: '@ls',
             hs: '@ls',
@@ -91,8 +96,8 @@ export default {
                 ag: "We're For-est For-est For-est Thump"
             }
         },
-        'Forest Thump Break 2': {
-            ls: 'X XX XX         X XX XX         X XX XX       X X XX XX X  X  X',
+        'Forest Thump (Draft), Break 2': {
+            ls: 'X XX XX         X XX XX         X XX XX         X XX XX X  X  X',
             ms: '@ls',
             hs: '@ls',
             re: '        XX XX X         XX XX X         XX XX X X XX XX X  X  X',
@@ -106,7 +111,7 @@ export default {
                 ag: repeat(3, "We are the for-est ") + "We're For-est For-est For-est Thump"
             }
         },
-        'Forest Thump Break 3': {
+        'Forest Thump (Draft), Break 3': {
             ls: 'X       XX XX X X             X X             X X XX XX X  X  X',
             ms: '@ls',
             hs: '@ls',
@@ -117,7 +122,7 @@ export default {
             sh: 'X             X X             X X             X X XX XX X  X  X',
             ot: '    A               A               A'
         },
-        'Forest Thump Break 5': {
+        'Forest Thump (Draft), Break 5': {
             ls: 'X             X X             X X             X X XX XX X  X  X',
             ms: '@ls',
             hs: '@ls',
