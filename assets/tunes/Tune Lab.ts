31619363617f27883089b624f95b3808ef13cad3
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2026-03-31 16:06:04 +0100

    Added Nova Balança, updated Forest Thump
    Following Drum Break 26

--- assets/tunes/Tune Lab.ts
+++ assets/tunes/Tune Lab.ts
@@ -1,4 +1,4 @@
-import {  repeat } from "../../src/tuneHelper";
+import {  repeat, crescendo } from "../../src/tuneHelper";
 
 export default {
 	displayName: "... from the Tune Lab",
@@ -72,15 +72,15 @@ export default {
             ls: 'X   r   XX XX X X   r   XX XX X X   r   XX XX X X   r   X  X  X',
             ms: 'X XX XX X   r X X XX XX X   r X X XX XX X   r X X   r   X  X  X',
             hs: '@ms',
-            re: 'X XX  f XX XXrf X XX  f XX XXrf X XX  f XX XXrf X   r f Xr Xr f',
-            sn: 'X.......X.....X X.......X.....X X.......X.....X X   r f XX XX X',
-            ta: 'X XX XX XX XX X X XX XX XX XX X X XX XX XX XX X X   r   XX XX X',
+            re: 'X XX  f XX XXrf X XX  f XX XXrf X XX  f XX XXrf X   r   Xr Xr f',
+            sn: 'X.......X.....X X.......X.....X X.......X.....X X   r   XX XX X',
+            ta: 'X XX XX X  X  X X XX XX X  X  X X XX XX X  X  X X   r   XX XX X',
             ag: 'o oa oa o     o o oa oa o     o o oa oa o     o o   r   oa oa o',
             sh: 'X...X...X...X...X...X...X...X...X...X...X...X...X       X  X  X',
             mnemonics: {
                 ls: repeat(3, "We like bang-ing our drums 'cause ") + "We are For-est Thump",
                 ms: repeat(3, "We can play all day long be-cause ") + "We are For-est Thump",
-				ta: repeat(3, "We all love the land, we are the for-est ") + "We are, we are the for-est",
+				ta: repeat(3, "We all love the land, For-est Thump ") + "We are, we are the for-est",
 				ag: repeat(3, "We can play all day long 'cause ") + "We are, we are the for-est"
             }
         },
@@ -135,11 +135,74 @@ export default {
             ls: 'X             X X             X X             X X XX XX X  X  X',
             ms: '@ls',
             hs: '@ls',
-            re: 'X f XXr XXr r X X rrr f XXrrr X X rX rX XXr r X X XX XX X  X  X',
+            re: 'X XX  f XX XXrf X XX  f XX XXrf X XX  f XX XXrf X XX XX X  X  X',
             sn: '@ls',
             ta: '@ls',
             ag: 'o             o o             o o             o o oa oa o  a  o',
             sh: '@ls'
-        }
+        },
+		'Nova Balanca': {
+			displayName: 'Nova Balança',
+			loop: true,
+			ls: 'X  X  XX      X ',
+			ms: '      XXX  X  X ',
+			hs: '@hs',
+			re: 'XX  X       X   ',
+			sn: '....X...XX..X...',
+			ta: 'X  XX X X  XX X ',
+			ag: 'o  oa o o  oa o ',
+			sh: '................',
+			mnemonics: {
+				ls: "O-pen it up Let's",
+				ms: "And let's shut it down",
+				re: "Flip it round round",
+				sn: "Back Push it back",
+				ta: "Turn it o-ver Turn it o-ver",
+				ag: "Turn it o-ver Turn it o-ver"
+			}
+		},
+		'Nova Balanca 1': {
+			displayName: 'Nova Balança, Break 1',
+			ls: 'X X X X X X X X XX              ',
+			ms: '@ls',
+			hs: '@ls',
+			re: '@ls',
+			sn: '@ls',
+			ta: '@ls',
+			ag: '@ls',
+			sh: '@ls',
+			volumeHack: crescendo(16),
+			mnemonics: { ls: "Shake it up from top to bo-ttom Flip it" }
+		},
+		'Nova Balanca 2': {
+			displayName: 'Nova Balança, Break 2',
+			ls: 'X   X   X   X   ',
+			ms: '@ls',
+			hs: '@ls',
+			re: '  X   X  X X  X ',
+			sn: '@re',
+			ta: '@re',
+			ag: '@re',
+			sh: '@re',
+			mnemonics: {
+				ls: "1   3   still        fix     ",
+				re: "  2   4       got to     more"
+			}
+		},
+		'Nova Balanca Call': {
+			displayName: 'Nova Balança, Call Break',
+			ls: repeat(2, '    X     X     '),
+			ms: '@ls',
+			hs: '@ls',
+			re: '@ls',
+			sn: repeat(2, 'XXXX  XXXX      '),
+			ta: '@ls',
+			ag: '@ls',
+			sh: '@ls',
+			mnemonics: {
+				ls: repeat(2, "                  up "),
+				sn: repeat(2, "Time to change it    ")
+			}
+		}
     }
 }
